9235ae6d8da245415894a677141fefbbbef5b192
andrey-khropov <5002794+andrey-khropov@users.noreply.github.com>
2025-03-15 23:09:02 +0100

    Don't check sdist

--- ci/build_all.py
+++ ci/build_all.py
@@ -633,7 +633,7 @@ def build_all(
             verbose,
             [
                 ['python3', 'setup.py', 'build_widget'],
-                ['python3', '-m', 'build', '--sdist']
+                #['python3', '-m', 'build', '--sdist']
             ]
         )
 
