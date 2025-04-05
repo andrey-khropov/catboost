084a57ab7d90ddfc7040e6b242a795030a0a500e
andrey-khropov <5002794+andrey-khropov@users.noreply.github.com>
2025-04-05 22:19:10 +0200

    Fix: set JAVA_HOME using native platform name

--- ci/build_all.py
+++ ci/build_all.py
@@ -314,7 +314,7 @@ def build_jvm_artifacts(
             logging.info(' '.join(cmd))
         if not dry_run:
             environ = copy.deepcopy(os.environ)
-            environ['JAVA_HOME'] = os.path.join(CMAKE_BUILD_ENV_ROOT, platform_name, JAVA_HOME)
+            environ['JAVA_HOME'] = os.path.join(CMAKE_BUILD_ENV_ROOT, get_native_platform_name(), JAVA_HOME)
             subprocess.check_call(cmd, env=environ)
 
 
